c1cd2e338698de2a8bab698484f62d57a3b9890c
buntman <ranmiem@gmail.com>
2023-10-25 22:18:30 +0800

    modified createOrder method

--- Menu.java
+++ Menu.java
@@ -20,58 +20,58 @@ public class Menu {
     int [] case_prices = {5000, 3500};
     List<orderItems> parts = new ArrayList<>();
 
-    public String displayCpu() {
+    public int displayCpu() {
         for (int i = 0; i < cpu.length; i++) {
             System.out.println((i + 1) + "-" + cpu[i] + " " + "₱" + cpu_prices[i]);
         }
         System.out.print("Enter choice: ");
         int choice = scan.nextInt();
-        return cpu[choice -1];
+        return choice -1;
     }
 
-    public String displayGpu() {
+    public int displayGpu() {
         for (int i = 0; i < gpu.length; i++) {
             System.out.println((i + 1) + "-" + gpu[i] + " " + "₱" + gpu_prices[i]);
         }
         System.out.print("Enter choice: ");
         int choice = scan.nextInt();
-        return gpu[choice -1];
+        return choice -1;
     }
 
-    public String displayStorage() {
+    public int displayStorage() {
         for (int i = 0; i < storage.length; i++) {
             System.out.println((i + 1) + "-" + storage[i] + " " + "₱" + storage_prices[i]);
         }
         System.out.print("Enter choice: ");
         int choice = scan.nextInt();
-        return storage[choice -1];
+        return  choice -1;
     }
 
-    public String displayMemory() {
+    public int displayMemory() {
         for (int i = 0; i < memory.length; i++) {
             System.out.println((i + 1) + "-" + memory[i] + " " + "₱" + memory_prices[i]);
         }
         System.out.print("Enter choice: ");
         int choice = scan.nextInt();
-        return memory[choice -1];
+        return choice -1;
     }
 
-    public String displayPsu() {
+    public int displayPsu() {
         for (int i = 0; i < psu.length; i++) {
             System.out.println((i + 1) + "-" + psu[i] + " " + "₱" + psu_prices[i]);
         }
         System.out.print("Enter choice: ");
         int choice = scan.nextInt();
-        return psu[choice -1];
+        return choice -1;
     }
 
-    public String displayCase() {
+    public int displayCase() {
         for (int i = 0; i < pc_case.length; i++) {
             System.out.println((i + 1) + "-" + pc_case[i] + " " + "₱" + case_prices[i]);
         }
         System.out.print("Enter choice: ");
         int choice = scan.nextInt();
-        return pc_case[choice -1];
+        return choice -1;
     }
 
 
@@ -79,37 +79,43 @@ public class Menu {
         System.out.println("                                                                                Welcome to SOLTECH! \n" +
                 "                                                                       Home of the best customized PC builds");
         System.out.println("CPU");
-        String cpu = displayCpu();
+        int cpu_index = displayCpu();
+        String cpu_choice = cpu[cpu_index];
+        int cpu_price = cpu_prices[cpu_index];
         System.out.println("GPU");
-        String gpu = displayGpu();
+        int gpu_index = displayGpu();
+        String gpu_choice = gpu[gpu_index];
+        int gpu_price = gpu_prices[gpu_index];
         System.out.println("Storage");
-        String storage = displayStorage();
+        int storage_index = displayStorage();
+        String storage_choice = storage[storage_index];
+        int storage_price = storage_prices[storage_index];
         System.out.println("Ram");
-        String memory = displayMemory();
+        int memory_index = displayMemory();
+        String memory_choice = memory[memory_index];
+        int memory_price = memory_prices[memory_index];
         System.out.println("PSU");
-        String psu = displayPsu();
+        int psu_index = displayPsu();
+        String psu_choice = psu[psu_index];
+        int psu_price = psu_prices[psu_index];
         System.out.println("Case");
-        String pc_case = displayCase();
-        parts.add(new orderItems(cpu, gpu, storage, memory,psu, pc_case));
+        int case_index = displayCase();
+        String case_choice = pc_case[case_index];
+        int case_price = case_prices[case_index];
+
+        parts.add(new orderItems(cpu_choice, gpu_choice, storage_choice, memory_choice,psu_choice, case_choice, cpu_price, gpu_price, storage_price, memory_price, psu_price, case_price));
     }
 
     public void listOfOrder() {
         for (orderItems items: parts) {
-            System.out.println("CPU: " + items.getCpu());
-            System.out.println("GPU: " + items.getGpu());
-            System.out.println("STORAGE: " + items.getStorage());
-            System.out.println("RAM: " + items.getMemory());
-            System.out.println("POWER SUPPLY: " + items.getPsu());
-            System.out.println("CASE: " + items.getPc_case());
+            System.out.println("CPU: " + items.getCpu() + " "+ "₱" + items.getCpu_price());
+            System.out.println("GPU: " + items.getGpu() + " "+ "₱" + items.getGpu_price());
+            System.out.println("STORAGE: " + items.getStorage() +" "+ "₱" + items.getStorage_price());
+            System.out.println("RAM: " + items.getMemory() +" "+ "₱" + items.getMemory_price());
+            System.out.println("POWER SUPPLY: " + items.getPsu() +" "+ "₱" + items.getPsu_price());
+            System.out.println("CASE: " + items.getPc_case() +" "+ "₱" + items.getCase_price());
+            System.out.println("Total Amount: " + "₱" +items.totalAmount());
         }
     }
-
-    public static void main(String[] args) {
-        Menu x1 = new Menu();
-        x1.createOrder();
-        x1.listOfOrder();
-    }
-
-
 
 }
